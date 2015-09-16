f9875c833b19cfe81bc846131dcf214ec4e6d5a9
ivanTaladro <ivanjfernandez@outlook.com>
2015-09-16 19:57:59 -0300

    Subo todo lo que tenemos de la web.

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){/* jQuery toggle layout */
+$('#btnToggle').click(function(){
+  if ($(this).hasClass('on')) {
+    $('#main .col-md-6').addClass('col-md-4').removeClass('col-md-6');
+    $(this).removeClass('on');
+  }
+  else {
+    $('#main .col-md-4').addClass('col-md-6').removeClass('col-md-4');
+    $(this).addClass('on');
+  }
+});
+});
+
+$(window).bind("load resize slid.bs.carousel", function() {
+  var imageHeight = $(".active .holder").height();
+  $(".controllers").height( imageHeight );
+  console.log("Slid");
+});
