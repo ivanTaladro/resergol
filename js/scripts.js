b2501054b2a6351fd3fafd5745dd6324cb6e2c2b
ivanTaladro <ivanjfernandez@outlook.com>
2015-09-19 22:28:48 -0300

    Modifico estilos de la pagina, y subo el Index.html 80% terminado.

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,23 @@ $(window).bind("load resize slid.bs.carousel", function() {
   $(".controllers").height( imageHeight );
   console.log("Slid");
 });
+
+$(document).ready(function() {
+  $(".carousel").carousel({
+    interval: 2000
+  });
+  $(".carousel").on("slid", function() {
+    var to_slide;
+    to_slide = $(".carousel-item.active").attr("data-slide-no");
+    $(".myCarousel-target.active").removeClass("active");
+    $(".carousel-indicators [data-slide-to=" + to_slide + "]").addClass("active");
+  });
+  $(".myCarousel-target").on("click", function() {
+    $(this).preventDefault();
+    $(".carousel").carousel(parseInt($(this).attr("data-slide-to")));
+    $(".myCarousel-target.active").removeClass("active");
+    $(this).addClass("active");
+  });
+});
+
+
