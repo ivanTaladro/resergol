93a6be2f313835a63d6612e1b236a0c0144e28bf
pedro <iralapedro@gmail.com>
2015-10-11 15:49:30 -0300

    subo los js

---
+++ js/estiloFiltrosCanchas.js
@@ -0,0 +1,43 @@
+$(document).ready(Pruebas);
+ 
+var contador = 1;
+ 
+function Pruebas () {
+	$('.container-fluid').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+ 
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+}
+
+
+function mostrar(){
+document.getElementById('oculto').style.display = 'block';}
+
+
+function ocultar(){
+document.getElementById('ver').style.display = 'none';}
+
+
+function muestra_oculta(id){
+if (document.getElementById){ //se obtiene el id
+var el = document.getElementById(id); //se define la variable "el" igual a nuestro div
+el.style.display = (el.style.display == 'none') ? 'block' : 'none'; //damos un atributo display:none que oculta el div
+}
+}
+window.onload = function(){/*hace que se cargue la función lo que predetermina que div estará oculto hasta llamar a la función nuevamente*/
+muestra_oculta('contenido_a_mostrar');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
+}
