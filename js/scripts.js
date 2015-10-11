34855ec6472b592e5f6aa9ae3712f62cba9f6a87
ivanTaladro <ivanjfernandez@outlook.com>
2015-10-11 16:55:07 -0300

    Modifico scripts.js por conflictos

--- js/scripts.js
+++ js/scripts.js
@@ -59,6 +59,26 @@ function init_map() {
 		var_marker.setMap(var_map);	
  
       }
+
+    function init_map() {
+		var var_location = new google.maps.LatLng(45.430817,12.331516);
+ 
+        var var_mapoptions = {
+          center: var_location,
+          zoom: 14
+        };
+ 
+		var var_marker = new google.maps.Marker({
+			position: var_location,
+			map: var_map,
+			title:"Venice"});
+ 
+        var var_map = new google.maps.Map(document.getElementById("map-containerIvan"),
+            var_mapoptions);
+ 
+		var_marker.setMap(var_map);	
+ 
+      }
  
       google.maps.event.addDomListener(window, 'load', init_map);
 
